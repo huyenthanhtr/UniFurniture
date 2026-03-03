6c81e6ee78392c18a2b0060c34e19dd2a724c07c
LeNhat <nhatltd23411@st.uel.edu.vn>
2026-03-03 17:48:59 +0700

    Hoàn thành module quản lý khuyến mãi cho UniFurniture

--- admin/src/app/app.config.ts
+++ admin/src/app/app.config.ts
@@ -1,11 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http'; // Thêm dòng này
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient() // Thêm vào mảng providers
   ]
 };
