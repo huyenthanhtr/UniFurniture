33132dc993fa8dfd28effe414f4f6b365e6c873e
huyenthanhtr <huyenthanhtr.xj@gmail.com>
2026-03-15 09:38:34 +0700

    Add order tracking page and review of client page (#41)

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -9,20 +9,20 @@ import { PromotionsPageComponent } from './pages/promotions/promotions';
 import { PolicyPageComponent } from './pages/policy/policy';
 import { AboutPageComponent } from './pages/about/about';
 import { CommunityPageComponent } from './pages/community/community';
+import { OrderTrackingComponent } from './pages/order-tracking/order-tracking';
 
 export const routes: Routes = [
-    { path: 'ar', component: ArViewer },
-    { path: 'ar-viewer', component: ArViewer },
-    { path: 'products', component: ProductComponent },
-    { path: 'products/:id', component: ProductDetailComponent },
-    { path: 'danh-muc', component: CategoryPageComponent },
-    { path: 'khuyen-mai', component: PromotionsPageComponent },
-    { path: 'tin-tuc', component: NewsPageComponent },
-    { path: 'tin-tuc/:slug', component: NewsDetailComponent },
-    { path: 'chinh-sach/:slug', component: PolicyPageComponent },
-    { path: 've-unifurniture', component: AboutPageComponent },
-    { path: 'cong-dong', component: CommunityPageComponent },
-    { path: 'cua-hang', redirectTo: 'cong-dong', pathMatch: 'full' },
+  { path: 'ar', component: ArViewer },
+  { path: 'ar-viewer', component: ArViewer },
+  { path: 'products', component: ProductComponent },
+  { path: 'products/:id', component: ProductDetailComponent },
+  { path: 'danh-muc', component: CategoryPageComponent },
+  { path: 'khuyen-mai', component: PromotionsPageComponent },
+  { path: 'tin-tuc', component: NewsPageComponent },
+  { path: 'tin-tuc/:slug', component: NewsDetailComponent },
+  { path: 'chinh-sach/:slug', component: PolicyPageComponent },
+  { path: 've-unifurniture', component: AboutPageComponent },
+  { path: 'cong-dong', component: CommunityPageComponent },
+  { path: 'cua-hang', redirectTo: 'cong-dong', pathMatch: 'full' },
+  { path: 'tra-cuu-van-don', component: OrderTrackingComponent },
 ];
-
-
